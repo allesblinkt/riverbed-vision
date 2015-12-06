592464b60482fd3bfa4dd74120cc1edec062ce2c
Benjamin Maus <benjamin@feld.is>
2015-12-06 22:21:58 +0100

    Range

--- brain/brain.py
+++ brain/brain.py
@@ -138,7 +138,7 @@ class Brain(object):
         # self.c.home()
 
     def start(self):
-       pass
+        pass
 
     def run(self, prgname):
         f = getattr(self, prgname)
@@ -152,7 +152,8 @@ class Brain(object):
         step = 100
         stones = []
         x, y = self.map.size
-        stepx, stepy = self.machine.cam.viewx / 2.0, self.machine.cam.viewy / 2.0
+        stepx = int(self.machine.cam.viewx / 2.0)
+        stepy = int(self.machine.cam.viewy / 2.0)
         for i in range(0, x + 1, stepx):
             for j in range(0, y + 1, stepy):
                 self.c.go(x=i, y=j)
