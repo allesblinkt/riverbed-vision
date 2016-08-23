63ed3406d6383f58a310f4437255374b6b1c0e3b
Benjamin Maus <benjamin@feld.is>
2016-08-23 19:34:51 +0200

    Minor cleanup

--- brain/utils.py
+++ brain/utils.py
@@ -14,23 +14,25 @@ def distance(a, b):
 
 
 def constrain(value, lower, upper):
+    """Clamps a value between lower and upper."""
     return max(lower, min(value, upper))
 
 
 def random_on_circle(rad):
+    """Generate a random angle 2D vector (tuple) with a specified length rad."""
     a = uniform(0, math.pi * 2.0)
     return (rad * math.sin(a), rad * math.cos(a))
 
 
 def map_value(value, start1, stop1, start2, stop2):
-    """
-        Simple scaling function
-    """
+    """Scale value from a range of start1, stop1, to a range of start2, stop2."""
     return start2 + (stop2 - start2) * ((value - start1) / float(stop1 - start1))
 
 
 def inkey():
-    import sys, tty, termios
+    import sys
+    import tty
+    import termios
 
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -40,3 +42,4 @@ def inkey():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
+
