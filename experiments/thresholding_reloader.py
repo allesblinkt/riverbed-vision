5497f537a8b2a50b24d08d9e2426083c8a7b559f
Benjamin Maus <benjamin@feld.is>
2016-08-28 22:18:49 +0200

    Testing

--- experiments/thresholding_reloader.py
+++ experiments/thresholding_reloader.py
@@ -7,9 +7,15 @@ import time
 import threshholding
 
 blank = cv2.imread('blank.png')
-orig_color_img = cv2.imread('../brain/map_offline/grab_1131_0621.jpg')
+orig_color_img = cv2.imread('../brain/map_offline/grab_0975_0828.jpg')
 # orig_color_img = cv2.imread('../brain/map_offline/grab_0000_0000.jpg')
-
+    # grab_0429_1380.jpg
+    # grab_0429_1380.jpg
+    # grab_1755_1173.jpg
+    # grab_1911_0276.jpg,
+#../brain/map_offline/grab_1131_0621.jpg
+    # FIXED
+    # grab_1365_1311.jpg
 
 print(blank.shape)
 print(orig_color_img.shape)
