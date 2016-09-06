6adc690d92dcff5a547f28923a30ace0405f04d2
Jller <river@allesblinkt.com>
2016-09-06 18:15:55 +0000

    Run mode

--- brain/brain.py
+++ brain/brain.py
@@ -458,9 +458,9 @@ class Brain(object):
         future_save.result() # wait until save is completed if still being done
 
 if __name__ == '__main__':
-    brain = Brain(use_machine=True, create_new_map=True)
+    brain = Brain(use_machine=True, create_new_map=False)
     brain.start()
     # brain.scan_from_files()
-    brain.scan(startx=1700, analyze=False)
+    # brain.scan(startx=1700, analyze=False)
     # brain.demo1()
-    #brain.performance()
+    brain.performance()
