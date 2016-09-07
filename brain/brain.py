a9dd8cc5525dd32bc0a4c2f5a3a460e7224065c3
Benjamin Maus <benjamin@feld.is>
2016-09-07 21:49:35 +0200

    Saving meta always

--- brain/brain.py
+++ brain/brain.py
@@ -464,7 +464,7 @@ class Brain(object):
 
     def save_map(self):
         self.save_map_wait()
-        self.future_save = executor_save.submit(self.stone_map.save)
+        self.future_save = executor_save.submit(self.stone_map.save, meta=True)
 
     def next_step(self):
         log.debug('Getting next step...')
