f53966f487e733ca9f9b003bb24faf3272d9db1b
Benjamin Maus <benjamin@feld.is>
2016-09-10 09:20:47 +0200

    Still move

--- brain/brain.py
+++ brain/brain.py
@@ -528,9 +528,9 @@ class Brain(object):
                     self.stone_map.move_stone(s, new_center=nc, angle=na)
                     self.stone_map.stage = stage  # Commit stage
 
-                    # putdown_pos = (nc[0], nc[1])
-                    # scan_pos = self.machine.cam.camera_pos_to_mm(putdown_pos)
-                    # self.m.go(x=scan_pos[0], y=scan_pos[1], e=90)
+                    putdown_pos = (nc[0], nc[1])
+                    scan_pos = self.machine.cam.camera_pos_to_mm(putdown_pos)
+                    self.m.go(x=scan_pos[0], y=scan_pos[1], e=90)
                     # self.scan_update()
 
                     log.info('Placement worked')
