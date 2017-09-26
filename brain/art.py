d511296585f67604a0d637feabc32368e27b507c
Benjamin Maus <info@allesblinkt.com>
2017-09-26 21:30:39 +0200

    Adapted workarea to current placement

--- brain/art.py
+++ brain/art.py
@@ -8,7 +8,7 @@ from log import makelog
 log = makelog(__name__)
 
 MAX_STAGE_MODE = 2  # How many stages / modes can we do (+1)
-WORKAREA_START_X = 2300
+WORKAREA_START_X = 2350
 
 flower_seeds = None
 # min_l, max_l = None, None    # Cache luma calculations
